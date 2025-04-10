0ead4efb5c2a5a808981221cfca1e1bd19580c24
Fuad Hasan <fuadhasan3503@gmail.com>
2025-04-10 16:10:27 +0600

    banner update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,22 +3,21 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div
-  className="hero"
-  style={{
-    backgroundImage: "url(https://i.ibb.co.com/HfSC293h/Banner-min.jpg)",
-  }}>
-  <div className="hero-overlay"></div>
-  <div className="text-neutral-content text-start p-40 grid place-items-start">
-    <div className="max-w-md">
-      <p className="text-4xl font-bold">
-      Bid on Unique Items from Around the World
+<div
+style={{
+  backgroundImage: "url(https://i.ibb.co.com/HfSC293h/Banner-min.jpg)",
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+}}
+>
+  <div className="w-11/12 mx-auto py-40 text-white">
+  <p className="text-6xl font-bold">
+      Bid on Unique Items from <br /> Around the World
       </p>
-      <p className='my-5'>
-      Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions
+      <p className='my-5 text-2xl font-light'>
+      Discover rare collectibles, luxury goods, and vintage <br /> treasures in our curated auctions
       </p>
       <button className="btn rounded-4xl ">Explore Auctions</button>
-    </div>
   </div>
 </div>
         </div>
